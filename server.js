f102c9006419745de7ceb77341f53859d71484bd
Thanmaya Silampur <silampurthanmaya@gmail.com>
2026-06-21 14:07:23 +0530

    made changes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("frontend"));
+app.use(express.static('.'));
 
 app.listen(port, function(){
     console.log("App running on http://localhost:" + port);
